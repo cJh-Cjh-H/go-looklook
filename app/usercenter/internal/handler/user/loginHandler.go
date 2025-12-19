3752007c92b8f537e931ad1fc1ac5129bdd4d427
陈佳豪 <13784638+chenjiahao0908@user.noreply.gitee.com>
2025-12-19 23:16:39 +0800

    gozer-look-look usercenter用户中心的api代码初步生成

---
+++ app/usercenter/internal/handler/user/loginHandler.go
@@ -0,0 +1,29 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"go-zero-looklook/internal/logic/user"
+	"go-zero-looklook/internal/svc"
+	"go-zero-looklook/internal/types"
+)
+
+// login
+func LoginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.LoginReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := user.NewLoginLogic(r.Context(), svcCtx)
+		resp, err := l.Login(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
